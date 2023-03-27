2ce2a3948f7512407824cea9e6f149e570614d98
Raj Kasaudhan <71208599+rajksd01@users.noreply.github.com>
2023-03-27 23:21:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Button from "./components/Button";
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <center>
+      
+        <Button/>
+        </center>
+       
+      </header>
+      <p className="footerIdentity">
+        <a href="http://www.rajkasaudhan.com.np"  target="_blank"  style={{textDecoration:"none", color:"black"}} rel="noreferrer"> Built with ♥ by Raj
+       </a>      </p>
+    </div>
+  );
+}
+
+export default App;
